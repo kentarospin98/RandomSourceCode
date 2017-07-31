927e18efc9a49dff0d64a8f5cdd80fc46f64d2d5
YoAbhay1 <30434988+YoAbhay1@users.noreply.github.com>
2017-07-31 19:09:20 +0530

    New Animation
    + Added New Loading Animations

--- NumberGame/sketch.js
+++ NumberGame/sketch.js
@@ -1,3 +1,11 @@
+$(document).ready(function() {
+
+    setTimeout(function(){
+        $('body').addClass('loaded');
+        $('h1').css('color','#222222');
+    }, 3000);
+
+});
 var walls;
 var players;
 var boardsize;
